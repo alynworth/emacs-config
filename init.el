d15e48f3c62bc63f1fcfdbced1bf5e5894e15793
Alan Charlesworth <ac@alans-macbook.home>
2016-01-30 09:24:28 +0000

    Initial commit of emacs config

---
+++ init.el
@@ -0,0 +1,29 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(package-initialize)
+(require 'use-package)
+
+;; Themeing
+(use-package gruvbox-theme)
+
+(use-package restart-emacs
+  :commands 'restart-emacs)
+
+
+;; Configuration 
+(setq mac-option-modifier 'meta)
+(setq mac-command-modifier 'super)
+(setq make-backup-files nil)
+
+(tool-bar-mode 0)
+(scroll-bar-mode 0)
+
+(set-face-attribute 'default nil :height 150)
+(toggle-frame-maximized)
+
+
+;; Keybindings
+(global-set-key (kbd "S-<tab>") 'hippie-expand) ;; `hippie-expand`
+						;; expands common
+						;; parts for auto
+						;; completion-ish
